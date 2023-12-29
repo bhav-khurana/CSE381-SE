decfcfc5f46ce9c409d3168ae9214abdaeb77665
Bhav Khurana <bhav.khurana.cse21@itbhu.ac.in>
2023-12-29 23:38:57 +0530

    Minor changes

--- exp1/convert.py
+++ exp1/convert.py
@@ -9,7 +9,7 @@ with open('exp1/output.json', 'r') as json_file:
 df = pd.DataFrame.from_dict(json_data, orient='index').transpose()
 
 # Write to Excel
-excel_file_path = 'user_story.xlsx'
+excel_file_path = 'exp1/user_story.xlsx'
 with pd.ExcelWriter(excel_file_path, engine='auto', mode='a') as writer:
     # Write to a new sheet
     df.to_excel(writer, sheet_name='userStory' + str(json_data['userStorySerialNumber']), index=False)
